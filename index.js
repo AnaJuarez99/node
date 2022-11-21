5d25a31d6f8e5e4e450b82e2f81ba881b6f01c22
Ana Juárez García <anajuarezz99@gmail.com>
2022-11-21 16:41:27 +0100

    segundoCommit
    segundo commit

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get('/', function(req,res){
     res.sendFile(ruta.join(__dirname, 'node_html/index.html'));
 });
 
+app.get('/hola', function(req,res){
+    res.send(`<h1>${req.query.name}</h1>`);
+});
+
 app.listen(9090);
